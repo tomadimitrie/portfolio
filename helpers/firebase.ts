6785af04690406d7a375c3d7cfe82c195e032b6a
Dimitrie-Toma Furdui <tomadimitrie@icloud.com>
2020-09-06 03:15:32 +0300

    updated to nextjs 9.5

--- helpers/firebase.ts
+++ helpers/firebase.ts
@@ -3,12 +3,12 @@ import "firebase/firestore";
 
 export default firebase.apps.length === 0
   ? firebase.initializeApp({
-      apiKey: process.env.firebase_api_key,
-      authDomain: process.env.firebase_auth_domain,
-      databaseURL: process.env.firebase_database_url,
-      projectId: process.env.firebase_project_id,
-      storageBucket: process.env.firebase_storage_bucket,
-      messagingSenderId: process.env.firebase_messaging_sender_id,
-      appId: process.env.firebase_app_id,
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      databaseURL: process.env.FIREBASE_DATABASE_URL,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID,
     })
   : firebase.app();
